795cfde24e46812099826668711c040ee1ae3d1e
jodydeanrogan <jodydeanrogan@yahoo.co.uk>
2017-06-15 11:01:14 +0100

    completed all controller actions

--- playschool/app/controllers/groups.rb
+++ playschool/app/controllers/groups.rb
@@ -2,16 +2,27 @@ Playschool::App.controllers :groups do
 
   layout :application
 
+  before :show, :edit, :update, :destroy do
+    @group = Group.find(params[:id])
+  end
+
   get :index do
+    @groups = Group.includes(:kids).all
     render 'index'
   end
 
   get :new, map: '/groups/new' do
+    @group = Group.new
     render 'new'
   end
 
   post :create do
-    redirect url_for(:groups, :show, id: @group.id)
+    @group = Group.new(params[:group])
+    if @group.save
+      redirect url_for(:groups, :show, id: @group.id)
+    else
+      redirect url_for(:groups, :new)
+    end
   end
 
   get :show, map: '/groups/:id' do
@@ -23,11 +34,19 @@ Playschool::App.controllers :groups do
   end
 
   put :update, map: '/groups/:id' do
-    redirect url_for(:groups, :show, id: @group.id)
+    if @group.update(params[:group])
+      redirect url_for(:groups, :show, id: @group.id)
+    else
+      redirect url_for(:groups, :edit, id: @group.id)
+    end
   end
 
   delete :destroy, map: '/groups/:id' do
-    redirect url_for(:groups, :index)
+    if @group.delete
+      redirect url_for(:groups, :index)
+    else
+      redirect url_for(:groups, :show, id: @group.id)
+    end
   end
 
 end
--- playschool/app/controllers/kids.rb
+++ playschool/app/controllers/kids.rb
@@ -2,16 +2,27 @@ Playschool::App.controllers :kids do
 
   layout :application
 
+  before :show, :edit, :update, :destroy do
+    @kid = Kid.find(params[:id])
+  end
+
   get :index do
+    @kids = Kid.includes(:group).all
     render 'index'
   end
 
   get :new, map: '/kids/new' do
+    @kid = Kid.new
     render 'new'
   end
 
   post :create do
-    redirect url_for(:kids, :show, id: @kid.id)
+    @kid = Kid.new(params[:kid])
+    if @kid.save
+      redirect url_for(:kids, :show, id: @kid.id)
+    else
+      redirect url_for(:kids, :new)
+    end
   end
 
   get :show, map: '/kids/:id' do
@@ -23,11 +34,19 @@ Playschool::App.controllers :kids do
   end
 
   put :update, map: '/kids/:id' do
-    redirect url_for(:kids, :show, id: @kid.id)
+    if @kid.update(params[:kid])
+      redirect url_for(:kids, :show, id: @kid.id)
+    else
+      redirect url_for(:kids, :edit, id: @kid.id)
+    end
   end
 
   delete :destroy, map: '/kids/:id' do
-    redirect url_for(:kids, :index)
+    if @kid.delete
+      redirect url_for(:kids, :index)
+    else
+      redirect url_for(:kids, :show, id: @kid.id)
+    end
   end
 
 end
